db6e75b23d7bd38ea6a127e7edbb89565c1641b8
yunantimh <yunantimogah@gmail.com>
2023-04-10 23:43:48 +0700

    pertama komit

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <>
+        <div className='container mx-auto'>
+            <div className='row'>
+                <div>
+                    <p>1</p>
+                </div>
+                <div>
+                    <h1>Yunanti Moga Hasanah</h1>
+                    <h2>Front-end Developer</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa nesciunt adipisci et, necessitatibus obcaecati ea perspiciatis, corrupti eligendi repellat ducimus voluptate ex eaque at iusto rerum perferendis? Beatae, quasi tenetur?</p>
+                    <button>Hubungi saya</button>
+                </div>
+                <div>
+                    <p>3</p>
+                </div>
+            </div>
+
+
+        </div>
+    </>
+  )
+}
